6441d94bc84bfcb8da57fffa3ec472456f9f8fc1
jusruizsa <sruizs.jr@gmail.com>
2022-12-15 16:28:49 -0500

    create component bid card

---
+++ components/BidCard.js
@@ -0,0 +1,20 @@
+app.component('bid-card', {
+    template:
+    /*html*/
+    `
+    <div class="bid-card">
+        <img src="https://i.pinimg.com/originals/f6/22/88/f622880615a333910cddad5311087936.jpg" alt="imagen" width="250">
+        <h5>PRICE</h5>
+        <p>
+            Lorem ipsum, dolor sit amet 
+            consectetur adipisicing elit. Ab 
+            nobis quae minima perspiciatis, 
+            voluptatum natus accusamus enim, 
+            consequuntur ad doloremque sunt 
+            quasi voluptates reiciendis distinctio 
+            libero, saepe nihil recusandae error? 
+        </p>
+        <button>PUJAR</button>
+    </div>
+    `
+})
