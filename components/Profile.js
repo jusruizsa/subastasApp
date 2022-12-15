0683b9c212097897ea739e715eb2fcf2a1ea7d5e
jusruizsa <sruizs.jr@gmail.com>
2022-12-15 16:59:03 -0500

    create product profile

---
+++ components/Profile.js
@@ -0,0 +1,10 @@
+app.component('profile', {
+    template:
+    /*html*/
+    `
+    <div class="profile">
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="imagen de perfil" width="200">
+        <span>Username</span>
+    </div>
+    `
+})
