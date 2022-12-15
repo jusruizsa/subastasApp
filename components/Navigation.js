599875a21a50acedae9446ebc60a2edd630ab107
jusruizsa <sruizs.jr@gmail.com>
2022-12-15 16:29:09 -0500

    create component Navigator

---
+++ components/Navigation.js
@@ -0,0 +1,12 @@
+app.component('navigation', {
+    template:
+    /*html*/
+    `
+    <Nav class="navigation">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Perfil</a></li> 
+        </ul>
+    </Nav>
+    `
+})
